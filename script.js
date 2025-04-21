2f09e2d531ceceaa949632aa7e2b07e7bd53ebcc
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-04-21 12:54:15 +0100

    Made adjustments on the structure and styling of the web page

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-"use strict";
-
 const form = document.querySelector("form");
 const email = document.getElementById("email");
-const subscribeButton = document.querySelector(".subscribe-btn");
+const dismissButton = document.querySelector(".dismiss-btn");
+const hoverStates = document.querySelectorAll(".hover-state");
+const desktopImageEdges = document.getElementById("a");
+
+hoverStates.forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    desktopImageEdges.style.rx = "0";
+  });
+
+  element.addEventListener("mouseleave", () => {
+    desktopImageEdges.style.rx = "1rem";
+  });
+})
+
+// Regular expression for email validation as per HTML specification
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (email.value.length !== 0 && emailRegExp.test(email.value)) {
+
+  }
+});
